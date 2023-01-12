d12317eb06d072d978c9da976a7824dfb10dada5
skytech001 <abiodunlabinjo@gmail.com>
2023-01-11 21:54:46 -0600

    deploying to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 // const publicPath = path.join(__dirname, "/build");
 
 app.use(express.static(path.join(__dirname, "/build")));
