b7424ba92a02373ce28cca67d7d2f3b79f07572c
skytech001 <abiodunlabinjo@gmail.com>
2023-01-11 21:26:21 -0600

    deploying to heroku

--- Server/server.js
+++ Server/server.js
@@ -2,11 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-const publicPath = path.join(__dirname, "..", "public");
+const __dirname = path.resolve();
+// const publicPath = path.join(__dirname, "/build");
 
-app.use(express.static(publicPath));
+app.use(express.static(path.join(__dirname, "/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 app.listen(port, () => {
   console.log(`server is up on port ${port}`);
