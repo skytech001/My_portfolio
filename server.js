b5fc5293e19a5b915a982f9d2f3565a0732cdf8c
skytech001 <abiodunlabinjo@gmail.com>
2023-01-11 22:14:01 -0600

    updating local startup

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
-// const __dirname = path.resolve();
-// const publicPath = path.join(__dirname, "/build");
 
 app.use(express.static(path.join(__dirname, "/build")));
 app.get("*", (req, res) => {
